eec22b13dd3c2b633b3973eab63cd7637977249b
tonkla2545 <suriyakamonkaro@gmail.com>
2023-06-09 14:57:28 +0700

    header

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use("*", (req,res,next) =>{
     next()
 })
 
-// app.use((req,res,next) =>{
-//     res.header('Access-Control-Allow-Origin','*')
-//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
-//     next()
-// })
+app.use((req,res,next) =>{
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 
 app.use(cors())
 
